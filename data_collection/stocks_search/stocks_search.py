51ab96c19cf0d7b516dbf05635b20640795d6a43
Ripolak <ros.jonatan98@gmail.com>
2023-04-17 22:28:37 +0300

    Refactor stock search

--- data_collection/stocks_search/stocks_search.py
+++ data_collection/stocks_search/stocks_search.py
@@ -1,6 +1,9 @@
 # This section might be used in the future.
 # Taken from: https://towardsdatascience.com/making-a-stock-screener-with-python-4f591b198261
+from typing import List
 
+import requests
+from bs4 import BeautifulSoup
 from pandas_datareader import data as pdr
 from yahoo_fin import stock_info as si
 import yfinance as yf
@@ -9,52 +12,50 @@ import time
 import csv
 
 
-def find_stocks():
-    yf.pdr_override()
+def get_sp500_companies_tickers() -> List[str]:
+    #  This is a very hacky and dirty way to get the companies, but it'll do for now
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    # Variables
-    tickers = si.tickers_dow()
-    tickers = [item.replace(".", "-") for item in tickers]  # Yahoo Finance uses dashes instead of dots
-    index_name = '^GSPC'  # S&P 500
-    start_date = datetime.datetime.now() - datetime.timedelta(days=365)
-    end_date = datetime.date.today()
-    export_list = []
+    sp500_table = soup.find("table", {"class": "wikitable"})
+    sp500_rows = sp500_table.find_all("tr")[1:]  # Skip the header row
 
-    returns_multiples = []
+    companies = []
+    for row in sp500_rows:
+        columns = row.find_all("td")
+        ticker = columns[0].text.strip()
+        name = columns[1].text.strip()
+        companies.append({"ticker": ticker, "name": name})
 
-    # Index Returns
-    index_df = pdr.get_data_yahoo(index_name, start_date, end_date)
-    index_df['Percent Change'] = index_df['Adj Close'].pct_change()
-    index_return = (index_df['Percent Change'] + 1).cumprod()[-1]
+    return [company['ticker'] for company in companies]
 
-    # Find top 30% performing stocks (relative to the S&P 500)
-    for ticker in tickers:
+
+index_symbols = {
+    'S&P 500': '^GSPC',
+    'NASDAQ 100': '^NDX',
+    'DOW 30': '^DJI',
+    # Add more index symbols here as needed
+}
+
+
+index_to_function = {
+    'S&P 500': get_sp500_companies_tickers(),
+    'DOW 30': si.tickers_dow(),
+}
+
+
+def get_stock_tickers(index: str) -> List[str]:
+    if index in index_to_function:
+        return index_to_function[index]
+    else:
+        raise ValueError(f"Index '{index}' not supported.")
+
+
+def perform_minervini_conditions_check(stocks_list: List, export_list: List, returns_multiples):
+    for stock, returns_multiple in stocks_list:
         try:
-            # Download historical data as CSV for each stock (makes the process faster)
-            df = pdr.get_data_yahoo(ticker, start_date, end_date)
-            df.to_csv(f'{ticker}.csv')
-
-            # Calculating returns relative to the market (returns multiple)
-            df['Percent Change'] = df['Adj Close'].pct_change()
-            stock_return = (df['Percent Change'] + 1).cumprod()[-1]
-
-            returns_multiple = round((stock_return / index_return), 2)
-            returns_multiples.append((ticker, returns_multiple))
-
-            print(f'Ticker: {ticker}; Returns Multiple against S&P 500: {returns_multiple}\n')
-            time.sleep(1)
-        except Exception:
-            print(f'Could not gather data on {ticker}')
-
-    # Creating a list of only top 30%
-    returns_multiples.sort(key=lambda x: x[1], reverse=True)
-    cutoff = int(len(returns_multiples) * 0.3)
-    top_stocks = returns_multiples[:cutoff]
-
-    # Checking Minervini conditions of top 30% of stocks in given list
-    for stock, returns_multiple in top_stocks:
-        try:
-            with open(f'{stock}.csv', 'r') as csvfile:
+            with open(f'csvs/{stock}.csv', 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
                 df = [row for row in reader]
 
@@ -110,5 +111,52 @@ def find_stocks():
             print(e)
             print(f"Could not gather data on {stock}")
 
+
+def find_stocks(index: str):
+    yf.pdr_override()
+
+    # Variables
+    tickers = get_stock_tickers(index)
+    tickers = [item.replace(".", "-") for item in tickers]  # Yahoo Finance uses dashes instead of dots
+    index_name = index_symbols[index]
+
+    start_date = datetime.datetime.now() - datetime.timedelta(days=365)
+    end_date = datetime.date.today()
+    export_list = []
+
+    returns_multiples = []
+
+    # Index Returns
+    index_df = pdr.get_data_yahoo(index_name, start_date, end_date)
+    index_df['Percent Change'] = index_df['Adj Close'].pct_change()
+    index_return = (index_df['Percent Change'] + 1).cumprod()[-1]
+
+    # Find top 30% performing stocks (relative to the S&P 500)
+    for ticker in tickers:
+        try:
+            # Download historical data as CSV for each stock (makes the process faster)
+            df = pdr.get_data_yahoo(ticker, start_date, end_date)
+            df.to_csv(f'csvs/{ticker}.csv')
+
+            # Calculating returns relative to the market (returns multiple)
+            df['Percent Change'] = df['Adj Close'].pct_change()
+            stock_return = (df['Percent Change'] + 1).cumprod()[-1]
+
+            returns_multiple = round((stock_return / index_return), 2)
+            returns_multiples.append((ticker, returns_multiple))
+
+            print(f'Ticker: {ticker}; Returns Multiple against S&P 500: {returns_multiple}\n')
+            time.sleep(1)
+        except Exception:
+            print(f'Could not gather data on {ticker}')
+
+    # Creating a list of only top 30%
+    returns_multiples.sort(key=lambda x: x[1], reverse=True)
+    cutoff = int(len(returns_multiples) * 0.3)
+    top_stocks = returns_multiples[:cutoff]
+
+    # Checking Minervini conditions of top 30% of stocks in given list
+    perform_minervini_conditions_check(top_stocks, export_list, returns_multiples)
+
     export_list.sort(key=lambda x: x['RS_Rating'], reverse=True)
-    print('\n', export_list)
+    return export_list
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from config import MAX_REPORTS_FOR_RECOMMENDATIONS
 from data_collection.news_collection.marketaux_collector import MarketauxNewsCollector
 from data_collection.news_collection.news_api_collector import NewsAPICollector
 from data_collection.stock_data_collection.company_stock_data_collector import get_stock_info
+from data_collection.stocks_search.stocks_search import find_stocks
 from export.export_purchase_recommendation_to_pdf import export_purchase_recommendation_to_pdf
 from export.export_stock_report_to_pdf import export_stock_report_to_pdf
 from recommendations_generator.get_recommendations import get_recommendations
@@ -14,38 +15,40 @@ from recommendations_generator.models import RiskPreference
 from stock_analysis.models import StockSymbolReport
 
 
-# if __name__ == '__main__':
-#     get_stock_info('MSFT')
-
 if __name__ == '__main__':
-    symbols = ["MSFT", "GOOGL", "INTC", "AMD", "EA", "ATVI", "TSLA", "AAPL", "UPS", "KOL", "NVDA", "ABR", "PTPI", "PARA"]
-    # stock_reports = []
-    # for symbol in symbols:
-    #     try:
-    #         report = generate_stock_report(symbol, days_ago_news=3)
-    #         stock_reports.append(report)
-    #     except Exception as e:
-    #         print(f'Failed to generate report for {symbol} with error: {e}')
-    files = os.listdir('exports/stock_symbol_reports')
-    stock_reports = [StockSymbolReport(**json.load(open(f'exports/stock_symbol_reports/{f}'))) for f in files]
-
-    current_situation = """
-    Here is my current portfolio CSV:
-    "Symbol","Description","Quantity","MarkPrice","FifoPnlUnrealized","OpenPrice","ReportDate"
-    "AAPL","APPLE INC","1","165.56","-1.22","166.78","2023-04-13"
-    "AMD","ADVANCED MICRO DEVICES","5","92.09","-10.9","94.27","2023-04-13"
-    "BBBY","BED BATH & BEYOND INC","102","0.2565","-13.334699","0.387202941","2023-04-13"
-    "GOOGL","ALPHABET INC-CL A","3","107.43","6.11","105.393333333","2023-04-13"
-    "INTC","INTEL CORP","10","32.13","-10.85","33.215","2023-04-13"
-    "MSFT","MICROSOFT CORP","2","289.84","6.48","286.6","2023-04-13"
-    "NVDA","NVIDIA CORP","4","264.63","-36.94","273.865","2023-04-13"
-    "VOO","VANGUARD S&P 500 ETF","24","379.77","85.048","376.226333333","2023-04-13"
-
-    I like technology and video games. I have about 4000 USD I want to invest. I want to invest for the short term to make a quick profit, not the long term. Make sure to make it diverse, I want 4-5 companies
-    """
-
-    stock_reports = stock_reports[:MAX_REPORTS_FOR_RECOMMENDATIONS]
-    recs = get_recommendations(stock_reports, current_situation, RiskPreference.RISKY)
-    export_purchase_recommendation_to_pdf(recs, 'recommendation.pdf')
-    print(recs.dict())
+    s = find_stocks('S&P 500')
+    print(s)
+    d = find_stocks('DOW 30')
+    print(d)
+# if __name__ == '__main__':
+#     symbols = ["MSFT", "GOOGL", "INTC", "AMD", "EA", "ATVI", "TSLA", "AAPL", "UPS", "KOL", "NVDA", "ABR", "PTPI", "PARA"]
+#     # stock_reports = []
+#     # for symbol in symbols:
+#     #     try:
+#     #         report = generate_stock_report(symbol, days_ago_news=3)
+#     #         stock_reports.append(report)
+#     #     except Exception as e:
+#     #         print(f'Failed to generate report for {symbol} with error: {e}')
+#     files = os.listdir('exports/stock_symbol_reports')
+#     stock_reports = [StockSymbolReport(**json.load(open(f'exports/stock_symbol_reports/{f}'))) for f in files]
+#
+#     current_situation = """
+#     Here is my current portfolio CSV:
+#     "Symbol","Description","Quantity","MarkPrice","FifoPnlUnrealized","OpenPrice","ReportDate"
+#     "AAPL","APPLE INC","1","165.56","-1.22","166.78","2023-04-13"
+#     "AMD","ADVANCED MICRO DEVICES","5","92.09","-10.9","94.27","2023-04-13"
+#     "BBBY","BED BATH & BEYOND INC","102","0.2565","-13.334699","0.387202941","2023-04-13"
+#     "GOOGL","ALPHABET INC-CL A","3","107.43","6.11","105.393333333","2023-04-13"
+#     "INTC","INTEL CORP","10","32.13","-10.85","33.215","2023-04-13"
+#     "MSFT","MICROSOFT CORP","2","289.84","6.48","286.6","2023-04-13"
+#     "NVDA","NVIDIA CORP","4","264.63","-36.94","273.865","2023-04-13"
+#     "VOO","VANGUARD S&P 500 ETF","24","379.77","85.048","376.226333333","2023-04-13"
+#
+#     I like technology and video games. I have about 4000 USD I want to invest. I want to invest for the short term to make a quick profit, not the long term. Make sure to make it diverse, I want 4-5 companies
+#     """
+#
+#     stock_reports = stock_reports[:MAX_REPORTS_FOR_RECOMMENDATIONS]
+#     recs = get_recommendations(stock_reports, current_situation, RiskPreference.RISKY)
+#     export_purchase_recommendation_to_pdf(recs, 'recommendation.pdf')
+#     print(recs.dict())
 
