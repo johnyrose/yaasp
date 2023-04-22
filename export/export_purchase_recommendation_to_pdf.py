92b255c944d820283a70c586346e80b4ac85a30e
Ripolak <ros.jonatan98@gmail.com>
2023-04-22 16:24:44 +0300

    Improve exported PDF

--- export/export_purchase_recommendation_to_pdf.py
+++ export/export_purchase_recommendation_to_pdf.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from pathlib import Path
 
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, ListFlowable, ListItem
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, ListFlowable, ListItem, TableStyle, Table
 from reportlab.lib.styles import getSampleStyleSheet
 from common.models.recommendations import PurchaseRecommendation
 from config import RECOMMENDATIONS_DIRECTORY
@@ -29,25 +30,42 @@ def export_purchase_recommendation_to_pdf(purchase_recommendation: PurchaseRecom
     content.append(explanation)
     content.append(Spacer(1, 12))
 
-    # Stock Recommendations List
-    recommendations_list = ListFlowable(
-        [
-            ListItem(
-                Paragraph(
-                    f"{recommendation.symbol}: Target Price {recommendation.target_price}, "
-                    f"Position {recommendation.position}, Amount {recommendation.amount} - "
-                    f"{recommendation.explanation}",
-                    styles['BodyText']
-                ),
-                leftIndent=18,
-            )
-            for recommendation in purchase_recommendation.stock_recommendations
-        ],
-        bulletType='bullet',
-        start='circle',
-    )
+    # Table Header
+    table_header = [
+        Paragraph('Symbol', styles['Heading3']),
+        Paragraph('Target Price', styles['Heading3']),
+        Paragraph('Position', styles['Heading3']),
+        Paragraph('Amount', styles['Heading3']),
+        Paragraph('Explanation', styles['Heading3'])
+    ]
 
-    content.append(recommendations_list)
+    # Stock Recommendations Table
+    recommendations_data = [table_header]
+
+    for recommendation in purchase_recommendation.stock_recommendations:
+        recommendation_data = [
+            Paragraph(recommendation.symbol, styles['BodyText']),
+            Paragraph(str(recommendation.target_price), styles['BodyText']),
+            Paragraph(recommendation.position, styles['BodyText']),
+            Paragraph(str(recommendation.amount), styles['BodyText']),
+            Paragraph(recommendation.explanation, styles['BodyText'])
+        ]
+        recommendations_data.append(recommendation_data)
+
+    recommendations_table = Table(recommendations_data, colWidths=[doc.width / 5.0] * 5)
+    recommendations_table.setStyle(TableStyle([
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+
+    content.append(recommendations_table)
     content.append(Spacer(1, 12))
 
     # Confidence Level
