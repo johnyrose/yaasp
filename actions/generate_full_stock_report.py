6f79b874766e85a114fc6a901bbe3c607561b283
Ripolak <ros.jonatan98@gmail.com>
2023-04-25 14:15:52 +0300

    Make self reflextion use the new news feature

--- actions/generate_full_stock_report.py
+++ actions/generate_full_stock_report.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 from typing import List
 
@@ -8,8 +7,7 @@ from common.openai_adapter import get_openai_response
 from common.openai_prompts import GET_SEARCH_TERMS_FOR_SELF_REFLEXION
 from config import GENERATING_NEWS_REPORT_MODEL
 from common.models.data_collection import NewsArticle, CompanyStockInfo
-from data_collection.news_collection.get_stock_news import get_stock_news
-from data_collection.news_collection.news_api_collector import NewsAPICollector
+from data_collection.news_collection.get_stock_news import get_stock_news, get_news_for_queries
 from data_collection.stock_data_collection.company_stock_data_collector import get_stock_info
 from common.models.stock_analysis import StockSymbolReport
 from stock_analysis.news_report_generator import generate_news_report
@@ -18,19 +16,12 @@ from stock_analysis.stock_symbol_report_generator import generate_stock_symbol_r
 
 def perform_self_reflexion(current_report: StockSymbolReport, current_news_articles: List[NewsArticle],
                            days_ago_news: int, stock_info: CompanyStockInfo) -> StockSymbolReport:
-    end_date = datetime.date.today()
-    start_date = end_date - datetime.timedelta(days=days_ago_news)
-
     prompt = GET_SEARCH_TERMS_FOR_SELF_REFLEXION.format(
         confidence_level_suggestions=current_report.confidence_explanation)
     response = get_openai_response(prompt, GENERATING_NEWS_REPORT_MODEL)
     search_terms = json.loads(response)
-    for term in search_terms:
-        try:
-            articles = NewsAPICollector(term).get_news_articles(num_articles=3, from_param=start_date)
-            current_news_articles += articles
-        except Exception as e:
-            logger.warning(f"Failed to get news for {term} with the following error: {e}")
+    new_articles = get_news_for_queries(search_terms, days_ago_news)
+    current_news_articles.extend(new_articles)
     updated_news_report = generate_news_report(current_report.stock_symbol, current_news_articles)
     new_full_report = generate_stock_symbol_report(current_report.stock_symbol, updated_news_report, stock_info)
     return new_full_report
@@ -39,7 +30,6 @@ def perform_self_reflexion(current_report: StockSymbolReport, current_news_artic
 def generate_full_stock_report(stock_symbol: str, days_ago_news: int = 5,
                                attempt_self_reflexion: bool = True) -> StockSymbolReport:
     stock_news = get_stock_news(stock_symbol, days_ago_news)
-    # marketaux_articles = MarketauxNewsCollector(stock_symbol).get_news_articles(start_date=start_date)
     stock_report = generate_news_report(stock_symbol, stock_news)
     stock_info = get_stock_info(stock_symbol)
     full_report = generate_stock_symbol_report(stock_symbol, stock_report, stock_info)
